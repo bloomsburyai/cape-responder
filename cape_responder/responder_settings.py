499e81b40b56e47fb43f98ff2a98d2bd16129cbb
Patrick Lewis <patrick@bloomsbury.ai>
2018-08-17 17:38:46 +0100

    environment variable tidy

--- cape_responder/responder_settings.py
+++ cape_responder/responder_settings.py
@@ -17,6 +17,9 @@ import os
 # FILE configuration
 THIS_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
+def envint(varname: str, default: int) -> int:
+   return int(os.getenv(varname, default))
+
 CLUSTER_SCHEDULER_IP = os.getenv("CAPE_CLUSTER_SCHEDULER_IP", "127.0.0.1")
-CLUSTER_SCHEDULER_PORT = int(os.getenv("CAPE_CLUSTER_SCHEDULER_PORT", 8786))
-NUM_WORKERS_PER_REQUEST = int(os.getenv("CAPE_NUM_WORKERS_PER_REQUEST", 8))
+CLUSTER_SCHEDULER_PORT = envint("CAPE_CLUSTER_SCHEDULER_PORT", 8786)
+NUM_WORKERS_PER_REQUEST = envint("CAPE_NUM_WORKERS_PER_REQUEST", 8)
